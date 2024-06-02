d5afd62cfffdc5e9f11d76303c23cf37e1b3a102
Emil Skogsund <emil.skogsund@gmail.com>
2024-06-02 12:51:50 +0200

    init

--- init.lua
+++ init.lua
@@ -37,6 +37,7 @@ https://github.com/folke/trouble.nvim
 
 -- Set <space> as the leader key
 -- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ','
 
