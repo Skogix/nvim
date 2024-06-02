11cf3269be812a647178f4ea304166487006b0c8
Emil Skogsund <emil.skogsund@gmail.com>
2024-06-02 18:05:39 +0200

    .

--- lua/skogix/plugins/vimwiki.lua
+++ lua/skogix/plugins/vimwiki.lua
@@ -1,17 +1,15 @@
 return {
   'vimwiki/vimwiki',
   branch = 'dev',
-  -- keys = { "<leader>x" },
-  config = function()
-    print 'Setting up vimwiki!'
+  event = 'BufEnter *.md',
+  init = function()
     vim.g.vimwiki_list = {
       {
-        template_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/vimwiki/autoload/',
+        path = '~/org/',
         syntax = 'markdown',
         ext = '.md',
-        path = '~/docs/personalwiki', -- does not work?=!?!?
       },
     }
-    vim.g.vimwiki_global_ext = 0
+    vim.g.vimwiki_ext2syntax = {}
   end,
 }
