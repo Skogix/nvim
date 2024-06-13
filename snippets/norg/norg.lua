13e7fd4cbbbad5927e3433dc6dc36759cd55d5ed
Emil Skogsund <emil.skogsund@gmail.com>
2024-06-13 11:39:46 +0200

    .

--- snippets/norg/norg.lua
+++ snippets/norg/norg.lua
@@ -2,7 +2,7 @@ local ls = require("luasnip")
 -- some shorthands...
 local s = ls.snippet
 local sn = ls.snippet_node
-local t = ls.text_node
+local t = ls.TextNode
 local i = ls.insert_node
 local f = ls.function_node
 local c = ls.choice_node
